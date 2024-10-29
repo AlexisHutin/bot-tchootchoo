ccabc1e58bbc8bbe2e06b63d33fe24b96759f96c
Alexis <alexis9653@hotmail.fr>
2024-10-29 22:54:28 +0100

    google sheet api connection test

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/AlexisHutin/bot-tchootchoo/cmd"
+import (
+	"github.com/AlexisHutin/bot-tchootchoo/cmd"
+	_ "github.com/joho/godotenv/autoload"
+)
 
 func main() {
 	cmd.Execute()
